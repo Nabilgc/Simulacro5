ffef6679c528ced0485fef5d27f8d69c7d4bea10
Nabil Galeb <nabilgaleb@hotmail.com>
2019-04-05 22:31:10 -0400

    Finalizado

---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIparcial.Models
+{
+    public enum UnityType
+    {
+        Litro,
+        Caja,
+        Botella,
+        Kg
+
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Required]
+
+        public UnityType Unit { get; set; }
+
+        public StatusType Status { get; set; }
+
+    }
+}
